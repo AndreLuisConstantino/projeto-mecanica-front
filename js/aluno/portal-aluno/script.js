f5758f6e7f9047ccebaa3fd40f5629fab68f37d6
Vinícius Monteiro <vmonteiro7843@gmail.com>
2023-06-02 16:56:29 -0300

    teas

--- js/aluno/portal-aluno/script.js
+++ js/aluno/portal-aluno/script.js
@@ -3,6 +3,7 @@
 const main = document.getElementById('main')
 const cardPerfil = document.querySelector('.card-perfil')
 const containerPerfil = document.querySelector('.modal')
+const buttonSalvar = document.querySelector('.save')
 
 
 
@@ -25,6 +26,14 @@ const exit = () => {
     
 }
 
+
+
+const save = () => {
+    
+    alert('PERFIL SALVO COM SUCESSO')
+    containerPerfil.style.display = 'none'
+}
 exit()
 
+buttonSalvar.addEventListener('click', save)
 cardPerfil.addEventListener('click', abrirContainerPerfil);
