a5f044ee8297140a1d1c51c5499e5a7e54200238
Usuário <andreluis081205@gmail.com>
2023-06-08 22:38:27 -0300

    aa

--- js/aluno/portal-aluno/script.js
+++ js/aluno/portal-aluno/script.js
@@ -5,25 +5,17 @@ const cardPerfil = document.querySelector('.card-perfil')
 const containerPerfil = document.querySelector('.modal')
 const buttonSalvar = document.querySelector('.save')
 
-
-
-
 const abrirContainerPerfil = () => {
-
     containerPerfil.style.display = 'flex'
-    
-
 }
 
 const exit = () => {
 
     const buttonFechar = document.querySelector('.quit')
 
-    buttonFechar.onclick = function() {
-
+    buttonFechar.onclick = () => {
         containerPerfil.style.display = 'none'
     }
-    
 }
 
 
